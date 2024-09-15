f933e9547f8266ab7f5b7c3830c475dd6f0a4e9b
Hasinarahmani <107977511+Hasinarahmani@users.noreply.github.com>
2024-09-15 20:24:31 +0430

    remove another error

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,19 +1,19 @@
-import { useSelector } from "react-redux";
-import RocketItem from "./RocketItem";
+import { useSelector } from 'react-redux';
+import RocketItem from ''./RocketItem';
 
 const Rocket = () => {
   const { rocketData, loading, error } = useSelector((state) => state.rocket);
 
   return (
     <div className="container mx-auto p-6 w-50px">
-      
+    
       {loading && <p className="text-center text-blue-500">Loading...</p>}
       {error && <p className="text-center text-red-500">Error! Unable to load data.</p>}
 
       {!loading && !error && (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8">
           {rocketData.map((rocket) => (
-            <RocketItem 
+            <RocketItem
               key={rocket?.id}
               id={rocket?.id}
               name={rocket?.name}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import  React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import store from './redux/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,5 +11,5 @@ root.render(
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </BrowserRouter>,
 );
