54733bbc88e3780e4e04237bc48af5743cfb25d5
Hasinarahmani <107977511+Hasinarahmani@users.noreply.github.com>
2024-09-15 19:17:19 +0430

    remove the linter error of app.js file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import './App.css';
-import Navbar from './components/NavBar';
 import { Route, Routes } from 'react-router-dom';
-import { useEffect } from "react";
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import Navbar from './components/NavBar';
 import Rocket from './components/Rocket';
 import Missions from './components/Missions';
 import Dragon from './components/Dragon';
 import MyProfile from './components/MyProfile';
 import { getDataFromServer } from './redux/rockets/rocketsSlice';
-import '../src/index.css'
+import './index.css';
 
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getDataFromServer())
-  },[dispatch])
+    dispatch(getDataFromServer());
+  }, [dispatch]);
   return (
     <div className="App">
-       <Navbar />
+      <Navbar />
       <Routes>
-        <Route path='/' element = {<Rocket />} />
-        <Route path='Missions' element = {<Missions />} />
-        <Route  path='Dragons' element = {<Dragon />} />
-        <Route path='myprofile' element = {<MyProfile />} />
+        <Route path="/" element={<Rocket />} />
+        <Route path="Missions" element={<Missions />} />
+        <Route path="Dragons" element={<Dragon />} />
+        <Route path="myprofile" element={<MyProfile />} />
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
