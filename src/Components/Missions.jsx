0acc76f40bf5ac042688489c71f021829bba6de4
Najeeba <najeebaqarqin@gmail.com>
2024-09-16 18:59:17 +0430

    Added the missions component and basic codes with simple tailwind css

---
+++ src/Components/Missions.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+const Missions = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Missions</h1>
+    </div>
+  );
+};
+
+export default Missions;
